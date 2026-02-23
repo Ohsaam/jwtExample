726c677b0dc9fc803ed2c61b18c4f9ff7fde858c
prototype <prototype>
2026-02-23 10:32:49 +0900

    Initial commit

---
+++ src/main/java/com/project/jwtmaster/JwtMasterApplication.java
@@ -0,0 +1,13 @@
+package com.project.jwtmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/jwtmaster/JwtMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jwtmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
